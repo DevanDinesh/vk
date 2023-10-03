5c0dab4e3fe5e18a38c6a23bcb479c682874f4b9
Devan Dinesh <devandinesh530@gmail.com>
2023-10-03 13:16:48 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/mighty/news/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mzadcom.veekshanam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/AdConfigurationConstants.dart
@@ -0,0 +1,39 @@
+bool isEnableAds = true;
+
+bool isOpenAdEnable = false;
+bool isBannerAdsEnable = true;
+bool isInterstitialAdsEnable = true;
+bool isAdEnableBetweenList = true;
+
+bool adEnableOnBookmark = false;
+bool adEnableOnShare = false;
+bool adEnableOnPlay = false;
+bool adEnableOnAddComment = false;
+bool adEnableOnChooseTopic = false;
+
+const AD_MOB_OPEN_AD_ID = "ca-app-pub-3949208513987489/2953138263";
+const AD_MOB_BANNER_ID = "ca-app-pub-3949208513987489/2940657838";
+const AD_MOB_INTERSTITIAL_ID = "ca-app-pub-3949208513987489/2953138263";
+const AD_MOB_REWARD_ID = "ca-app-pub-3949208513987489/1448484903";
+
+const AD_MOB_OPEN_AD_ID_IOS = "ca-app-pub-3949208513987489~2673357308";
+const AD_MOB_BANNER_ID_IOS = "ca-app-pub-3949208513987489/1150476355";
+const AD_MOB_INTERSTITIAL_ID_IOS = "ca-app-pub-3949208513987489/9647913475";
+const AD_MOB_REWARD_ID_IOS = "ca-app-pub-3949208513987489/9852723763";
+
+String faceBookBannerPlacementId = "ADD YOUR FACEBOOK BANNER PLACEMENT ID";
+String faceBookRewardPlacementId = "ADD YOUR FACEBOOK REWARD PLACEMENT ID";
+String faceBookInterstitialPlacementId = "ADD YOUR FACEBOOK INTERSTITIAL PLACEMENT ID";
+
+String banner = "banner";
+String interstitial = "interstitial";
+String reward = "rewarded";
+String native = 'native';
+
+String admob = "admob";
+String facebookAudience = "Facebook";
+
+String mDisplayBanner = admob;
+String mDisplayInterstitial = admob;
+String mDisplayReward = admob;
+String mDisplayNative = admob;
